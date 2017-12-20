6d8627df1099894641894184c35edabe91d6afb2
Jose Antonio Aguilar <jaguilar992@gmail.com>
2017-12-20 12:23:24 -0600

    Beta2

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -5,15 +5,20 @@ import java.util.ResourceBundle;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.KeyCode;
 import model.Atributo;
 import model.Documento;
 import model.Metodo;
@@ -21,6 +26,9 @@ import model.TipoAcceso;
 import model.TipoDocumento;
 
 public class Controller implements Initializable{
+	private int selectedMetodo  = -1;
+	private int selectedAtributo = -1;
+	
 	private Documento mainDoc = new Documento();
 	//private Documento importDoc = new Documento();
 
@@ -28,7 +36,8 @@ public class Controller implements Initializable{
 	private ObservableList<Atributo> listaAtributos;
 	private ObservableList<Metodo> listaMetodos;
 	private ObservableList<TipoDocumento> listaTipoDocumento;
-	private ObservableList<TipoAcceso> listaTipoAcceso;
+	private ObservableList<TipoAcceso> listaTipoAccesoAtributos;
+	private ObservableList<TipoAcceso> listaTipoAccesoMetodos;
 
 	//Componentes
 		//	TableView
@@ -50,11 +59,16 @@ public class Controller implements Initializable{
 	@FXML Button btnSetAtributo;
 	@FXML Button btnDelAtributo;
 	@FXML Button btnDelAllAtributos;
+	@FXML Button btnEditAtributo;
 	
 	@FXML Button btnAddMetodo;
 	@FXML Button btnSetMetodo;
 	@FXML Button btnDelMetodo;
 	@FXML Button btnDelAllMetodos;
+	@FXML Button btnEditMetodo;
+
+	@FXML Button btnCleanEditor;
+	@FXML Button btnGenerate;
 	
 		//	TextArea
 	@FXML TextArea txtEditor;
@@ -63,6 +77,10 @@ public class Controller implements Initializable{
 	@FXML TextField txtIdentificadorAtributo;
 	@FXML TextField txtDefaultAtributo;
 	@FXML TextField txtIdentificadorMetodo;
+		//	CheckBox
+	@FXML CheckBox cbxConstruct;
+	@FXML CheckBox cbxToString;
+	@FXML CheckBox cbxGetSet;
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
@@ -70,23 +88,25 @@ public class Controller implements Initializable{
 		listaAtributos = FXCollections.observableArrayList();
 		listaMetodos = FXCollections.observableArrayList();
 		listaTipoDocumento = FXCollections.observableArrayList();
-		listaTipoAcceso = FXCollections.observableArrayList();
+		listaTipoAccesoAtributos = FXCollections.observableArrayList();
+		listaTipoAccesoMetodos = FXCollections.observableArrayList();
 		
 		//Llenar elementos en Listas
 		this.llenarTipoDocumentos(listaTipoDocumento);
-		this.llenarTipoAcceso(listaTipoAcceso);
+		this.llenarTipoAcceso(listaTipoAccesoAtributos, 0);
+		this.llenarTipoAcceso(listaTipoAccesoMetodos, 1);
 				
 		// Enlazar listas con TableViews
 		tblAtributos.setItems(listaAtributos);
 		tblMetodos.setItems(listaMetodos);
 		cmbTipoDocumento.setItems(listaTipoDocumento);
-		cmbTipoAccesoAtributo.setItems(listaTipoAcceso);
-		cmbTipoAccesoMetodo.setItems(listaTipoAcceso);
+		cmbTipoAccesoAtributo.setItems(listaTipoAccesoAtributos);
+		cmbTipoAccesoMetodo.setItems(listaTipoAccesoMetodos);
 		
 		//Seleccionar primer elemento de la lista
 		cmbTipoDocumento.getSelectionModel().select(listaTipoDocumento.get(0));
-		cmbTipoAccesoAtributo.getSelectionModel().select(listaTipoAcceso.get(0));
-		cmbTipoAccesoMetodo.getSelectionModel().select(listaTipoAcceso.get(0));
+		cmbTipoAccesoAtributo.getSelectionModel().select(listaTipoAccesoAtributos.get(0));
+		cmbTipoAccesoMetodo.getSelectionModel().select(listaTipoAccesoMetodos.get(2));
 		
 		// Enlazar Columnas de TableViews
 		colAtributoAcceso.setCellValueFactory(new PropertyValueFactory<Atributo,String>("acceso"));
@@ -97,8 +117,66 @@ public class Controller implements Initializable{
 		colMetodoIdentificador.setCellValueFactory(new PropertyValueFactory<Metodo, String>("identificador"));
 		
 		
-		//Manejar Modelo
+		// EVENTOS
+		//ENTER EN ID ATRIB
+		txtIdentificadorAtributo.setOnKeyPressed((event) -> { 
+			if(event.getCode() == KeyCode.ENTER) { 
+				if (selectedAtributo==-1) {
+					addItemAtributos();
+				}else {
+					setItemAtributos();
+				}
+		}});
+		// ENTER EN ID DEFAULT
+		txtDefaultAtributo.setOnKeyPressed((event) -> {
+			if(event.getCode() == KeyCode.ENTER) { 
+				if (selectedAtributo==-1) {
+					addItemAtributos();
+				}else {
+					setItemAtributos();
+				}
+		}});
+		// ENTER EN MET ATRIB
+		txtIdentificadorMetodo.setOnKeyPressed((event) -> { 
+			if(event.getCode() == KeyCode.ENTER) { 
+				if (selectedMetodo!=-1) {
+					addItemMetodos();
+				}else {
+					addItemMetodos();
+				}
+		}});
+		// TABLE VIEW
+		tblAtributos.setOnKeyPressed((event)->{
+			int index = tblAtributos.getSelectionModel().getSelectedIndex();
+			if (index!=-1) {
+				switch (event.getCode()) {
+					case DELETE:
+						listaAtributos.remove(index);
+					break;
+					case ENTER:
+						EditAtributo();
+					break;
+					default:break;
+				}
+				
+			}
+		});
 		
+		tblMetodos.setOnKeyPressed((event)->{
+			int index = tblMetodos.getSelectionModel().getSelectedIndex();
+			if(index!=-1) {
+				switch (event.getCode()) {
+					case DELETE:
+						listaMetodos.remove(index);
+					break;
+					case ENTER:
+						EditMetodo();
+					break;
+					default:break;
+				}
+				
+			}
+		});
 	}
 	
 	public void llenarTipoDocumentos(ObservableList<TipoDocumento> lista) {
@@ -107,12 +185,14 @@ public class Controller implements Initializable{
 		lista.add(new TipoDocumento(Documento.TIPO_INTERFAZ, "Interfaz"));
 	}
 	
-	public void llenarTipoAcceso(ObservableList<TipoAcceso> lista) {
+	public void llenarTipoAcceso(ObservableList<TipoAcceso> lista, int type) {
 		lista.add(new TipoAcceso(Documento.TIPO_ACCESO_PRIVATE, "private"));
 		lista.add(new TipoAcceso(Documento.TIPO_ACCESO_PROTECTED, "protected"));
 		lista.add(new TipoAcceso(Documento.TIPO_ACCESO_PUBLIC, "public"));
-		lista.add(new TipoAcceso(Documento.TIPO_ACCESO_CONST, "const"));
 		lista.add(new TipoAcceso(Documento.TIPO_ACCESO_STATIC, "static"));
+		if(type==0) {
+			lista.add(new TipoAcceso(Documento.TIPO_ACCESO_CONST, "const"));
+		}
 	} 
 	
 	//Agregar elementos
@@ -121,9 +201,29 @@ public class Controller implements Initializable{
 		TipoAcceso acceso = cmbTipoAccesoAtributo.getSelectionModel().getSelectedItem();
 		String identificador = txtIdentificadorAtributo.getText();
 		String default_valor = txtDefaultAtributo.getText();
-		Atributo nuevo = new Atributo(acceso, identificador, default_valor);
-		listaAtributos.add(nuevo);
-		mainDoc.addAtributo(nuevo);
+		if(!identificador.equals("")) {
+			if(
+				(acceso.getId()==Documento.TIPO_ACCESO_CONST || 
+				acceso.getId()==Documento.TIPO_ACCESO_STATIC) &&
+				default_valor.equals("")
+			) {
+				Alert alert = new Alert(AlertType.WARNING);
+				alert.setTitle("Alerta");
+				alert.setHeaderText("No se encontró valor por defecto");
+				alert.setContentText("Atributo "+acceso.getNombre()+", necesita un valor.");
+				alert.showAndWait();
+			}else {
+				Atributo nuevo = new Atributo(acceso, identificador, default_valor);
+				listaAtributos.add(nuevo);
+				mainDoc.addAtributo(nuevo);
+				cleanFormAtributos();
+			}
+		}else {
+			Alert alert = new Alert(AlertType.WARNING);
+			alert.setTitle("Alerta");
+			alert.setContentText("Defina un identificador.");
+			alert.showAndWait();
+		}
 		
 	}
 	
@@ -131,11 +231,76 @@ public class Controller implements Initializable{
 	public void addItemMetodos() {
 		TipoAcceso acceso = cmbTipoAccesoMetodo.getSelectionModel().getSelectedItem();
 		String identificador = txtIdentificadorMetodo.getText();
-		Metodo nuevo = new Metodo(acceso, identificador);
-		listaMetodos.add(nuevo);
-		mainDoc.addMetodo(nuevo);
-		cleanFormAtributos();
+		if(!identificador.equals("")) {
+			Metodo nuevo = new Metodo(acceso, identificador);
+			listaMetodos.add(nuevo);
+			mainDoc.addMetodo(nuevo);
+			cleanFormMetodos();
+		}else {
+			Alert alert = new Alert(AlertType.WARNING);
+			alert.setTitle("Alerta");
+			alert.setContentText("Defina un identificador.");
+			alert.showAndWait();
+		}
 	}
+	
+	
+	//Actualizar elementos
+		@FXML
+		public void setItemAtributos() {
+			TipoAcceso acceso = cmbTipoAccesoAtributo.getSelectionModel().getSelectedItem();
+			String identificador = txtIdentificadorAtributo.getText();
+			String default_valor = txtDefaultAtributo.getText();
+			if(!identificador.equals("")) {
+				if(
+					(acceso.getId()==Documento.TIPO_ACCESO_CONST || 
+					acceso.getId()==Documento.TIPO_ACCESO_STATIC) &&
+					default_valor.equals("")
+				) {
+					Alert alert = new Alert(AlertType.WARNING);
+					alert.setTitle("Alerta");
+					alert.setHeaderText("No se encontró valor por defecto");
+					alert.setContentText("Atributo "+acceso.getNombre()+", necesita un valor.");
+					alert.showAndWait();
+				}else {
+					Atributo actualizado = new Atributo(acceso, identificador, default_valor);
+					listaAtributos.set(selectedAtributo, actualizado);
+					mainDoc.setAtributo(selectedAtributo, actualizado);
+					cleanFormAtributos();
+					btnSetAtributo.setDisable(true);
+					btnAddAtributo.setDisable(false);
+					btnEditAtributo.setDisable(false);
+					selectedAtributo=-1;
+				}
+			}else {
+				Alert alert = new Alert(AlertType.WARNING);
+				alert.setTitle("Alerta");
+				alert.setContentText("Defina un identificador.");
+				alert.showAndWait();
+			}
+			
+		}
+		
+		@FXML
+		public void setItemMetodos() {
+			TipoAcceso acceso = cmbTipoAccesoMetodo.getSelectionModel().getSelectedItem();
+			String identificador = txtIdentificadorMetodo.getText();
+			if(!identificador.equals("")) {
+				Metodo actualizado = new Metodo(acceso, identificador);
+				listaMetodos.set(selectedMetodo, actualizado);
+				mainDoc.setMetodo(selectedMetodo, actualizado);
+				cleanFormMetodos();
+				btnSetMetodo.setDisable(true);
+				btnAddMetodo.setDisable(false);
+				btnEditMetodo.setDisable(false);
+				selectedMetodo=-1;
+			}else {
+				Alert alert = new Alert(AlertType.WARNING);
+				alert.setTitle("Alerta");
+				alert.setContentText("Defina un identificador.");
+				alert.showAndWait();
+			}
+		}
 	
 	//Borrar elementos
 	@FXML
@@ -143,6 +308,7 @@ public class Controller implements Initializable{
 		int item = tblAtributos.getSelectionModel().getSelectedIndex();
 		if(item!=-1) {
 			listaAtributos.remove(item);
+			mainDoc.deleteAtributo(item);
 		}
 	}
 	@FXML
@@ -150,6 +316,7 @@ public class Controller implements Initializable{
 		int item = tblMetodos.getSelectionModel().getSelectedIndex();
 		if(item!=-1) {
 			listaMetodos.remove(item);
+			mainDoc.deleteMetodo(item);
 		}
 	}
 	
@@ -166,17 +333,73 @@ public class Controller implements Initializable{
 		mainDoc.clearListaMetodos();
 	}
 	
+	// Editar Atributos
+	@FXML
+	public void EditAtributo() {
+		selectedAtributo = tblAtributos.getSelectionModel().getSelectedIndex();
+		if(selectedAtributo!=-1) {
+			btnSetAtributo.setDisable(false);
+			btnAddAtributo.setDisable(true);
+			btnEditAtributo.setDisable(true);
+			Atributo selec = tblAtributos.getSelectionModel().getSelectedItem();
+			
+			txtIdentificadorAtributo.setText(selec.getIdentificador());
+			txtDefaultAtributo.setText(selec.getValorDefault());
+			cmbTipoAccesoAtributo.getSelectionModel().select(selec.getAcceso());
+			
+		}
+	}
+	
+	//Editar Metodos
+	@FXML
+	public void EditMetodo() {
+		selectedMetodo = tblMetodos.getSelectionModel().getSelectedIndex();
+		if(selectedMetodo!=-1) {
+			btnSetMetodo.setDisable(false);
+			btnAddMetodo.setDisable(true);
+			btnEditMetodo.setDisable(true);
+			Metodo selec = tblMetodos.getSelectionModel().getSelectedItem();
+			
+			txtIdentificadorMetodo.setText(selec.getIdentificador());
+			cmbTipoAccesoMetodo.getSelectionModel().select(selec.getAcceso());
+			
+		}
+	}
+	
+	
 	//Limpiar entradas
 	//Atributos
 	public void cleanFormAtributos() {
 		cmbTipoAccesoAtributo.getSelectionModel().select(0);
 		txtIdentificadorAtributo.clear();
 		txtDefaultAtributo.clear();
-	}
-	public void cleanFormMetodos() {
-		cmbTipoAccesoMetodo.getSelectionModel().select(0);
-		txtIdentificadorMetodo.clear();
+		txtIdentificadorAtributo.requestFocus();
 	}
 	//Metodos
+	public void cleanFormMetodos() {
+		cmbTipoAccesoMetodo.getSelectionModel().select(2);
+		txtIdentificadorMetodo.clear();
+		txtIdentificadorMetodo.requestFocus();
+	}
+
+	// LIMPIAR EDITOR
+	@FXML
+	public void clearEditor() {
+		txtEditor.clear();
+	}
+	
+	@FXML
+	public void generarDoc() {
+		if(!txtNombreDocumento.getText().equals("")) {
+			mainDoc.setNameDoc(txtNombreDocumento.getText());
+			mainDoc.setConstruct(cbxConstruct.isSelected());
+			mainDoc.setGetAndSet(cbxGetSet.isSelected());
+			mainDoc.setTostring(cbxToString.isSelected());
+			txtEditor.setText(mainDoc.generar());
+		}
+	}
 	
 }
+
+
+
--- src/model/Documento.java
+++ src/model/Documento.java
@@ -3,6 +3,13 @@ package model;
 import java.util.ArrayList;
 
 public class Documento {
+	private boolean construct = false;
+	private boolean tostring = false;
+	private boolean getAndSet = false;
+	private String path="";
+	private String nameDoc="";
+	
+	
 	//Tipos Documento
 	public static final int TIPO_CLASE = 0;
 	public static final int TIPO_CLASE_ABSTRACTA = 1;
@@ -17,12 +24,12 @@ public class Documento {
 	private ArrayList<Atributo> listaAtributos;
 	private ArrayList<Metodo> listaMetodos;
 	
-	private String path="";
-	private String nameDoc="";
 	
 	// TAGS
 	private final String TAG_INIT = "<?php"; 
 	private final String TAG_END = "?>";
+	private final String TAB = "\t";
+	private final String END = "\n";
 	
 	public Documento() {
 		this.listaAtributos = new ArrayList<>();
@@ -37,6 +44,23 @@ public class Documento {
 		listaMetodos.add(m);
 	}
 	
+	public void deleteMetodo(int m) {
+		listaMetodos.remove(m);
+	}
+	
+	public void deleteAtributo(int a) {
+		listaAtributos.remove(a);
+	}
+	
+	public void setMetodo(int i, Metodo m) {
+		listaMetodos.set(i, m);
+	}
+	
+	public void setAtributo(int i, Atributo a) {
+		listaAtributos.set(i, a);
+	}
+	
+	
 	public void clearListaAtributos() {
 		this.listaAtributos.clear();
 	}
@@ -44,5 +68,226 @@ public class Documento {
 	public void clearListaMetodos() {
 		this.listaMetodos.clear();
 	}
+
+
+	public void setConstruct(boolean construct) {
+		this.construct = construct;
+	}
+
+
+	public void setTostring(boolean tostring) {
+		this.tostring = tostring;
+	}
+
 	
+	public void setGetAndSet(boolean getAndSet) {
+		this.getAndSet = getAndSet;
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+
+	public String getNameDoc() {
+		return nameDoc;
+	}
+
+	public void setNameDoc(String nameDoc) {
+		this.nameDoc = nameDoc;
+	}
+
+	public ArrayList<Atributo> getListaAtributos() {
+		return listaAtributos;
+	}
+
+	public ArrayList<Metodo> getListaMetodos() {
+		return listaMetodos;
+	}
+	// Generador
+	public String generar() {
+		String doc ="";
+		doc+=TAG_INIT+END;
+		doc+="class "+nameDoc+"{"+END;
+		if(listaAtributos.size()>0) {
+			doc+=genAtr();
+			doc+=END;
+		}
+		if (construct) {
+			doc+=genConstruct();
+			doc+=END;
+		}
+		if(tostring) {
+			doc+=genToString();
+			doc+=END;
+					}
+		
+		if(getAndSet) {
+			doc+=genGetSet();
+			doc+=END;
+		}
+		
+		if (listaMetodos.size()>0) {
+			doc+=genMetodos();
+			doc+=END;
+		}
+		
+		doc+="}"+END;
+		doc+=TAG_END;
+		return doc;
+	}
+	
+	public String genAtr() {
+		String buffer="";
+		for (Atributo a : listaAtributos) {
+			boolean valor_default = !a.getValorDefault().equals("");
+			switch (a.getAcceso().getId()) {
+			case TIPO_ACCESO_PUBLIC:
+				buffer+=TAB+"public $";
+				buffer+=a.getIdentificador();
+				buffer+= (!construct && valor_default) ? " = "+a.getValorDefault()+";"+END: ";"+END;
+			break;
+			case TIPO_ACCESO_PRIVATE:
+				buffer+=TAB+"private $";
+				buffer+=a.getIdentificador();
+				buffer+= (!construct && valor_default) ? " = "+a.getValorDefault()+";"+END: ";"+END;
+			break;
+			case TIPO_ACCESO_PROTECTED:
+				buffer+=TAB+"protected $";
+				buffer+=a.getIdentificador();
+				buffer+= (!construct && valor_default) ? " = "+a.getValorDefault()+";"+END: ";"+END;
+			break;
+			case TIPO_ACCESO_CONST:
+				buffer+=TAB+"public const ";
+				buffer+=a.getIdentificador();
+				buffer+=" = "+a.getValorDefault()+";"+END;
+			break;
+			case TIPO_ACCESO_STATIC:
+				buffer+=TAB+"public static $";
+				buffer+=a.getIdentificador();
+				buffer+=" = "+a.getValorDefault()+";"+END;
+			break;
+			default:
+			break;
+			}
+		}
+		return buffer;
+	}
+	
+	public String genConstruct() {
+		String buffer="";
+		//buffer+=TAB+"#Constructor de la clase."+END;
+		//buffer+=TAB+"#Soporta \"Sobrecarga\" secuencial."+END;
+		buffer+=TAB+"public function __construct(";
+		int j = 0;
+		for (int i=0; i<listaAtributos.size(); i++) {
+			Atributo a = listaAtributos.get(i);
+			if ((
+				a.getAcceso().getId()!=TIPO_ACCESO_CONST &&
+				a.getAcceso().getId()!=TIPO_ACCESO_STATIC
+			)) {
+				boolean val = !a.getValorDefault().equals("");
+				buffer+=(j!=0) ? ",":"";
+				buffer+=END+TAB+TAB;
+				buffer+="$"+a.getIdentificador()+" = ";
+				buffer+= (val) ? a.getValorDefault() :"null";
+				j++;
+			}
+		}
+		buffer+=END+TAB+"){";
+		buffer+=END;
+		for (Atributo a : listaAtributos) {
+			if((
+				a.getAcceso().getId()!=TIPO_ACCESO_CONST &&
+				a.getAcceso().getId()!=TIPO_ACCESO_STATIC	
+			)) {
+				buffer+=TAB+TAB+"$this->"+a.getIdentificador()+" = $"+a.getIdentificador()+";"+END;
+			}
+		}
+		buffer+=TAB+"}";
+		return buffer+END;
+		
+	}
+	
+	public String genToString() {
+		String buffer="";
+		//buffer+=TAB+"#__toString()"+END;
+		buffer+=TAB+"public function __toString(){"+END;
+		buffer+=TAB+TAB+"$var = \""+nameDoc+"{\"";
+		int j=0;
+		for (Atributo a:listaAtributos) {
+			if((
+				a.getAcceso().getId()!=TIPO_ACCESO_CONST &&
+				a.getAcceso().getId()!=TIPO_ACCESO_STATIC	
+			)) {
+				buffer+=(j!=0)?".\" , \"":"";
+				buffer+=END+TAB+TAB;
+				buffer+=".\""+a.getIdentificador()+": \"."+"$this->"+a.getIdentificador();
+				j++;
+			}
+		}
+		buffer+=";"+END;
+		buffer+=TAB+TAB+"return $var.\"}\";"+END;
+		buffer+=TAB+"}";
+		return buffer+END;
+	}
+
+	public String genGetSet() {
+		String buffer="";
+		for (Atributo a:listaAtributos) {
+			if((
+				a.getAcceso().getId()!=TIPO_ACCESO_PUBLIC &&
+				a.getAcceso().getId()!=TIPO_ACCESO_CONST &&
+				a.getAcceso().getId()!=TIPO_ACCESO_STATIC
+			)) {
+				String id = a.getIdentificador();
+				buffer+=TAB+"public function get"+title(id)+"(){"+END;
+				buffer+=TAB+TAB+"return $this->"+id+";"+END;
+				buffer+=TAB+"}"+END;
+				buffer+=END;
+				buffer+=TAB+"public function set"+title(id)+"($"+id+"){"+END;
+				buffer+=TAB+TAB+"$this->"+id+" = $"+id+";"+END;
+				buffer+=TAB+"}"+END;
+			}
+		}
+		return buffer;
+	}
+	
+	
+	public String genMetodos() {
+		String buffer="";
+		for(Metodo m : listaMetodos) {
+			String id = m.getIdentificador();
+			switch (m.getAcceso().getId()) {
+			case TIPO_ACCESO_PUBLIC:
+				buffer+=TAB+"public ";
+			break;
+			case TIPO_ACCESO_STATIC:
+				buffer+=TAB+"public static ";
+			break;
+			case TIPO_ACCESO_PRIVATE:
+				buffer+=TAB+"private ";
+			break;
+			case TIPO_ACCESO_PROTECTED:
+				buffer+=TAB+"protected ";
+			break;
+			default:break;
+			}
+			buffer+="function "+id+"(/*Parametros*/){";
+			buffer+=END+TAB+"}"+END;
+		}
+		return buffer;
+	}
+	
+	
+	
+	public String title(String original) {
+	    if (original == null || original.length() == 0) {
+	        return original;
+	    }
+	    return original.substring(0, 1).toUpperCase() + original.substring(1);
+	}
 }
